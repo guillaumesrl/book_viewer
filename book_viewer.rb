6c6d79fcdc5110cc0f55ef3d87a7f48b48ff3710
guillaumesrl <guillaumesrl@gmail.com>
2021-08-17 22:33:34 +0200

    add search function

--- book_viewer.rb
+++ book_viewer.rb
@@ -12,15 +12,35 @@ helpers do
   end
 end
 
+
+
+def search_query(query)
+  return nil if query.nil?
+  (1..@toc.size).select { |chap_num| File.read("data/chp#{chap_num}.txt").downcase.match?(query.downcase) }
+end
+
+
+
 get "/" do
   @title = "homepage"
   erb :home
 end
 
 get "/chapters/:num" do
+  num = params[:num].to_i
+  redirect "/" unless num.to_s == params[:num] && (1..@toc.size).include?(num)
+
   @chapter = File.read("data/chp#{params[:num]}.txt")
-  @title = "chapter #{params[:num]} : #{File.readlines("data/toc.txt")[params[:num].to_i-1]}"
+  @title = "chapter #{params[:num]} : #{File.readlines("data/toc.txt")[num-1]}"
 
   erb :chapter
 end
 
+not_found do
+  redirect "/", 301
+end
+
+get "/search" do
+  @result_arr = search_query(params[:query])
+  erb :search
+end
